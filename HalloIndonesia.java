051e251badac11432191f7a5c75375975a60bf64
Ojiel-exe <78788238+Ojiel-exe@users.noreply.github.com>
2021-03-21 19:07:59 +0700

    Add files via upload

---
+++ HalloIndonesia.java
@@ -0,0 +1,16 @@
+/**
+ * @Nama  : Aditya Satrio Priyandi
+ * @Nim   : 1120093000010
+ * @Kelas : 2D
+ */
+
+
+
+public class HalloIndonesia {
+    public static void main(String[] args) {
+        System.out.println("Halo Jakarta");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");               
+    }
+}
+    
